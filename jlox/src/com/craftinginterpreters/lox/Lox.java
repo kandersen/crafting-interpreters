67be3ec12119cd7ecc3acc5184488f0d1b492386
Kristoffer Andersen <kja@cs.au.dk>
2019-12-06 18:37:35 +0100

    [jlox] Completed chapter 7 - interpreting expressions
    - Introduced Interpreter

--- jlox/src/com/craftinginterpreters/lox/Lox.java
+++ jlox/src/com/craftinginterpreters/lox/Lox.java
@@ -9,8 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -28,6 +29,7 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
 
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -50,7 +52,7 @@ public class Lox {
 
         if (hadError) return;
 
-        System.out.println(new ASTPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -70,4 +72,8 @@ public class Lox {
         hadError = true;
     }
 
+    public static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
+    }
 }
